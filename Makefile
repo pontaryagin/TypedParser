55b9678af087f6a8fd134bb6898afa0547b3d8ca
pontaryagin <ponta1496@gmail.com>
2021-09-04 16:15:23 +0900

    add Pair

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
 
 build:
 	g++ main.cpp --std=c++2a -o main.out -g3 -Wall -Wextra
+
+run: build
+	./main.out
--- main.cpp
+++ main.cpp
@@ -6,51 +6,53 @@ using String = std::string;
 
 namespace tp{
 
+template<typename T>
+concept HasRetType = requires(T){ typename T::ret_type; };
+
 template<typename T, char Sep>
 struct Vec
 {
 	using ret_type = std::vector<T>;
 };
 
-template<typename T, char Sep>
-requires requires (T) { typename T::ret_type; }
+template<HasRetType T, char Sep>
 struct Vec<T, Sep>
 {
 	using ret_type = std::vector<typename T::ret_type>;
 };
 
-
-
-// template<typename T, typename Sep>
-// struct Pair
-// {
-// 	using ret_type = std::pair<T>;
-// };
-
-template<char separator>
-struct Sep
+template<typename S, typename T, char sep>
+struct Pair
 {
-	static constexpr char value = separator;
+	using ret_type = std::pair<S, T>;
+};
+
+template<typename S, HasRetType T, char sep>
+struct Pair<S, T, sep>
+{
+	using ret_type = std::pair<S, typename T::ret_type>;
+};
+
+template<HasRetType S, typename T, char sep>
+struct Pair<S, T, sep>
+{
+	using ret_type = std::pair<typename S::ret_type, T>;
+};
+
+template<HasRetType S, HasRetType T, char sep>
+struct Pair<S, T, sep>
+{
+	using ret_type = std::pair<typename S::ret_type, typename T::ret_type>;
 };
 
 template<typename T>
 struct Parser{};
-
 
 template<>
 struct Parser<double>
 {
 	static double parse(char* start, char* end) 
 	{
-		// double value;
-    // if (auto [ptr, ec] = std::from_chars(start, end, value); ec == std::errc{}) {
-    //   std::cout << value << std::endl;
-    // }
-    // else {
-    //   throw std::invalid_argument("Invalid format for double : "s + start);
-    // }
-		// return value;
-
 		auto last_char = *end;
 		*end = '\0';
 		char* check;
@@ -60,6 +62,7 @@ struct Parser<double>
 			*end = last_char;
 			throw std::invalid_argument("Invalid format for double : "s + start);
 		}
+		*end = last_char;
 		return parsed;
 	}
 };
@@ -78,6 +81,20 @@ struct Parser<int>
 			*end = last_char;
 			throw std::invalid_argument("Invalid format for double : "s + start);
 		}
+		*end = last_char;
+		return parsed;
+	}
+};
+
+template<>
+struct Parser<String>
+{
+	static String parse(char* start, char* end)
+	{
+		auto last_char = *end;
+		*end = '\0';
+		auto parsed = String(start);
+		*end = last_char;
 		return parsed;
 	}
 };
@@ -90,14 +107,7 @@ struct Parser<Vec<T, Sep>>
 	{
 		// constexpr auto separator = Sep::value;
 		constexpr auto separator = Sep;
-		size_t cnt = 0;
-		for (auto it = start; it < end; ++it)
-		{
-			if (*it == separator)
-			{
-				++cnt;
-			}
-		}
+		size_t cnt = std::count(start, end, Sep);
 		typename _wholeType::ret_type parsed;
 		parsed.reserve(cnt + 1);
 		auto curr_start = start;
@@ -114,7 +124,22 @@ struct Parser<Vec<T, Sep>>
 	}
 };
 
-#include <memory>
+template<typename S, typename T, char Sep>
+struct Parser<Pair<S, T, Sep>>
+{
+	using _wholeType = Pair<S, T, Sep>;
+	static typename _wholeType::ret_type parse(char* start, char* end)
+	{
+		size_t cnt = std::count(start, end, Sep);
+		auto pos = std::find(start, end, Sep);
+		if (cnt != 1)
+		{
+			throw std::invalid_argument("Invalid format for Pair : "s + start);
+		}
+		return {Parser<S>::parse(start, pos), Parser<T>::parse(pos+1, end)};
+	}
+};
+
 template<typename T>
 auto parse(const String& str)
 {
@@ -122,13 +147,23 @@ auto parse(const String& str)
 	str.copy(s.get(), str.size());
 	return Parser<T>::parse(s.get(), s.get() + str.size());
 }
+
+template<typename T>
+auto parse(const char* str)
+{
+	auto len = std::strlen(str);
+	auto s = std::make_unique<char[]>(len+1);
+	std::strcpy(s.get(), str);
+	return Parser<T>::parse(s.get(), s.get() + len);
+}
+
 }
 
 int main() {
-	using T= tp::Vec<tp::Vec<int,':'>, ';'>;
 
 	String s = "12:23;124:34";
+	using T= tp::Vec<tp::Pair<String, int,':'>, ';'>;
 	auto d = tp::parse<T>(s);
-	
+
 	return 0;
 }
